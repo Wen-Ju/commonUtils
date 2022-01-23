fe8df57eee9cc8f2bd06f0314368f3fde098a2d3
juwen <834580508@qq.com>
2022-01-23 18:26:35 +0800

    工具类

---
+++ .idea/utils/IpUtils.java
@@ -0,0 +1,91 @@
+package com.jd.omni.membership.domain.util;
+
+
+
+import com.jd.omni.membership.common.exception.SystemException;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+/**
+ * @author zaomeng
+ * @date 2021/6/2 9:00 下午
+ * @description
+ */
+public class IpUtils {
+
+    /**
+     * IP地址的正则表达式.
+     */
+    public static final String IP_REGEX = "((\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3})";
+
+    private static volatile String cachedIpAddress;
+
+    /**
+     * 获取本机IP地址.
+     *
+     * <p>
+     * 有限获取外网IP地址.
+     * 也有可能是链接着路由器的最终IP地址.
+     * </p>
+     *
+     * @return 本机IP地址
+     */
+    public static String getIp() {
+        if (null != cachedIpAddress) {
+            return cachedIpAddress;
+        }
+        Enumeration<NetworkInterface> netInterfaces;
+        try {
+            netInterfaces = NetworkInterface.getNetworkInterfaces();
+        } catch (final SocketException ex) {
+            throw new SystemException("001","当前机器上所有的网络接口",ex);
+        }
+        String localIpAddress = null;
+        while (netInterfaces.hasMoreElements()) {
+            NetworkInterface netInterface = netInterfaces.nextElement();
+            Enumeration<InetAddress> ipAddresses = netInterface.getInetAddresses();
+            while (ipAddresses.hasMoreElements()) {
+                InetAddress ipAddress = ipAddresses.nextElement();
+                if (isPublicIpAddress(ipAddress)) {
+                    String publicIpAddress = ipAddress.getHostAddress();
+                    cachedIpAddress = publicIpAddress;
+                    return publicIpAddress;
+                }
+                if (isLocalIpAddress(ipAddress)) {
+                    localIpAddress = ipAddress.getHostAddress();
+                }
+            }
+        }
+        cachedIpAddress = localIpAddress;
+        return localIpAddress;
+    }
+
+    private static boolean isPublicIpAddress(final InetAddress ipAddress) {
+        return !ipAddress.isSiteLocalAddress() && !ipAddress.isLoopbackAddress() && !isV6IpAddress(ipAddress);
+    }
+
+    private static boolean isLocalIpAddress(final InetAddress ipAddress) {
+        return ipAddress.isSiteLocalAddress() && !ipAddress.isLoopbackAddress() && !isV6IpAddress(ipAddress);
+    }
+
+    private static boolean isV6IpAddress(final InetAddress ipAddress) {
+        return ipAddress.getHostAddress().contains(":");
+    }
+
+    /**
+     * 获取本机Host名称.
+     *
+     * @return 本机Host名称
+     */
+    public static String getHostName() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (final UnknownHostException ex) {
+            throw new SystemException("002","获取主机名称异常",ex);
+        }
+    }
+}
